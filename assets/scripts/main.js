e0147a442d70e600bb8cce62c2fe7da556d39e64
Cheree <71246795+Chere3@users.noreply.github.com>
2022-10-26 22:36:31 -0500

    Primera y unica commit.

---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+// Inicia el gradiente de fondo.
+
+var gradient = new Gradient()
+gradient.initGradient('#gradient-canvas');
+
+// Cambiar de color el boton al poner el cursor encima.
+var button = document.getElementById('button');
+
